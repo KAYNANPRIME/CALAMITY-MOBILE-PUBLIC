15649796a46560c7787e0aca7fb8f6af3d3784fd
KAYNAN <166578926+KAYNANPRIME@users.noreply.github.com>
2024-04-10 01:01:44 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,99 @@
+import { Npcsattack } from './Resources/npcs/Npcsattack.js';
+
+import { complexweapons } from './weapons/complexweapons.js';
+
+import { bosscomplex } from './bosses/bosscomplex.js';
+
+import { biomemobs } from './Resources/npcs/biomemobs.js';
+
+import { projectile } from './itemresource/projectile.js';
+
+import { desert } from './bosses/desert.js';
+
+import { aqua } from './bosses/aqua.js';
+
+import { nonpc } from './Resources/npcs/nonpc.js';
+
+import { storm } from './bosses/storm.js';
+
+import { stormtail } from './bosses/stormtail.js';
+
+import { stormhead } from './bosses/stormhead.js';
+
+import { bossconfig } from './bosses/Bosscofig.js';
+
+import { hitbox } from './Resources/npcs/hitbox.js';
+
+import { drop } from './Resources/npcs/drop.js';
+
+import { hitproj } from './itemresource/hitproj.js';
+
+import { modify } from './itemresource/projmodify.js';
+
+import { Astra } from './bosses/Astra.js';
+
+import { plague } from './bosses/plague.js';
+
+import {registro} from "./Registro/registro.js";
+
+import { Yharon } from './bosses/Yharon.js';
+
+//Item Mod Settings 
+
+import { damage } from './itemresource/itemdamage.js';
+
+import { recentita } from './itemresource/recentita.js';
+
+//Localization
+
+import { credits } from './Resources/Global/Globalcredits.js';
+
+import { shop } from './Resources/Global/Shop.js';
+
+import { wiki } from './Resources/Global/wiki.js';
+
+//Minions Settings
+
+import { Deathstare } from './Resources/minion/Deathstare.js';
+
+import { FrostBlossom } from './Resources/minion/FrostBlossom.js';
+
+import { CinderBlossom } from './Resources/minion/CinderBlossom.js';
+
+import { sunspirit } from './Resources/minion/sunspirit.js';
+
+import { TundraBlossoms } from './Resources/minion/TundraBlossoms.js';
+
+import { Vengeful } from './Resources/minion/Vengeful.js';
+
+Npcsattack();
+complexweapons();
+bosscomplex();
+biomemobs();
+projectile();
+desert();
+aqua();
+nonpc();
+storm();
+stormtail();
+stormhead();
+hitbox();
+drop();
+hitproj();
+bossconfig();
+modify();
+Astra();
+plague();
+Yharon();
+damage();
+recentita();
+credits();
+shop();
+wiki();
+registro();
+Deathstare();
+FrostBlossom();
+CinderBlossom();
+sunspirit();
+TundraBlossoms();
+Vengeful();
